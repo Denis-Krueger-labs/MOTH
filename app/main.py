2e0ae5f4db5784a1b8eeb5367ab6f9b8df0a1b0b
Denis-Krueger-labs <denis.krueger@study.thws.de>
2026-09-16 19:52:16 +0200

    mof found the api

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {
+        "name": "MOTH",
+        "status": "awake",
+        "message": "mof found the api",
+    }
